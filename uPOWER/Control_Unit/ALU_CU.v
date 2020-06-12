5942389fbc893ca1f05a73bd6ae2ab53fba13937
niranjansy <niranjan.sy99@gmail.com>
2020-06-13 00:34:36 +0530

    Update uPOWER ALU CU

--- uPOWER/Control_Unit/ALU_CU.v
+++ uPOWER/Control_Unit/ALU_CU.v
@@ -1,18 +1,15 @@
 module ALU_CU(in_signal, ALU_OP);
 
-    input[5:0] in_signal;
+    input[3:0] in_signal;
     output[3:0] ALU_OP;
 
-    assign a = in_signal[5];
-    assign b = in_signal[4];
-    assign c = in_signal[3];
-    assign d = in_signal[2];
-    assign e = in_signal[1];
-    assign f = in_signal[0];
+    assign m = in_signal[3];
+    assign n = in_signal[2];
+    assign p = in_signal[1];
+    assign q = in_signal[0];
 
-    assign ALU_OP[3] = a & e & f;
-    assign ALU_OP[2] = (~a & b) | (a & e);
-    assign ALU_OP[1] = ~a | ~d;
-    assign ALU_OP[0] = (a & ~e & f) | (a & c);
-
+    assign ALU_OP[3] = m & (~n) & p;
+    assign ALU_OP[2] = q & (m ^ n);
+    assign ALU_OP[1] = (~m & n) | (p ^ q);
+    assign ALU_OP[0] = (~n & ~q) | (p & q & m);
 endmodule
