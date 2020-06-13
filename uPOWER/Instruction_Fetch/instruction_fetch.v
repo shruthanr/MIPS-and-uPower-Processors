3965de8b81be45d1ff73ea3f844d9d5b6f9f6bb9
shruthanr <shruthan@outlook.com>
2020-06-14 01:53:11 +0530

    Add Instruction Fetch to RILS

--- uPOWER/Instruction_Fetch/instruction_fetch.v
+++ uPOWER/Instruction_Fetch/instruction_fetch.v
@@ -1,13 +1,13 @@
 module Instruction_Memory
 (
-    input [31:0] curr_line,
+    input [63:0] curr_line,
     output [31:0] curr_instr
 );
 
     reg [31:0] instruction_memory [0:100];
     initial
     begin
-        $readmemb("instructions.mem", instruction_memory);
+        $readmemb("./Instruction_fetch/instructions.mem", instruction_memory);
     end
 
     assign curr_instr = instruction_memory[curr_line]; 
@@ -21,9 +21,9 @@ module Instruction_Fetch
     output [31:0] curr_instr
 );
 
-    wire [31:0] PC, new_PC;
+    wire [63:0] PC, new_PC;
     wire [31:0] curr_instr;
-    wire [31:0] curr_line;
+    wire [63:0] curr_line;
 
     reg clk;
 
@@ -34,10 +34,11 @@ module Instruction_Fetch
         forever #20 clk = ~clk;
     end
 
-    ProgramCounter p(.clk(clk), .rst(rst), .out_PC(PC), .in_PC(new_PC));
+    // ProgramCounter p(.clk(clk), .rst(rst), .out_PC(PC), .in_PC(new_PC));
     // Update_PC upc(PC, new_PC, Branch, zero_flag, immediate);
+    ProgramCounter p(.clk(clk), .rst(rst), .PC(PC));
     
-    reg [31:0] offset = 00000000000001000000000000000000;
+    reg [63:0] offset = 0000000000000000000000000000000000000000000001000000000000000000;
     assign curr_line = (PC - offset)/4;
 
     Instruction_Memory M(.curr_line(curr_line), .curr_instr(curr_instr));
@@ -45,60 +46,42 @@ module Instruction_Fetch
 endmodule
 
 /* module Instruction_Fetch_tb;
-
     reg clk = 1'b0;
     reg rst = 1'b0;
     wire [31:0] curr_instr;
-
     always #100 clk = ~clk;
-
     Instruction_Fetch I(.rst(rst), .clk(clk), .curr_instr(curr_instr));
-
     initial
     begin
         rst = 1;
         #500;
-
         rst = 0;
         #100;
-
         rst = 0;
         #100;
-
         rst = 0;
         #100;
-
         rst = 0;
         #100;
-
         rst = 0;
         #100;
-
         rst = 0;
         #100;
-
         rst = 0;
         #100;
-
         rst = 0;
         #100;
-
         rst = 0;
         #100;
-
         rst = 0;
         #100;
-
         rst = 0;
         #100;
     end
-
     initial
     begin 
         $monitor("Current instruction is %32b at time %d", curr_instr, $time);
-
         #10000;
         $finish;
     end
-
 endmodule */
--- uPOWER/Instruction_Fetch/program_counter.v
+++ uPOWER/Instruction_Fetch/program_counter.v
@@ -1,34 +1,37 @@
 module Update_PC
 (
-    input [31:0] PC,
-    output [31:0] new_PC,
-    input Branch, zero_flag,
-    input [31:0] immediate
+    input [63:0] PC,
+    input clk,
+    output [63:0] new_PC
+    
+    // input Branch, zero_flag,
+    // input [31:0] immediate
 );
+    assign new_PC = PC + 4;
+    // wire [31:0] Branch_Target;
 
-    wire [31:0] Branch_Target;
+    // assign Branch_Target = (immediate << 2) + PC + 4;
 
-    assign Branch_Target = (immediate << 2) + PC + 4;
-
-    Mux_2_1_32 m4(PC+4, Branch_Target, (zero_flag & Branch), new_PC);
+    // Mux_2_1_32 m4(PC+4, Branch_Target, (zero_flag & Branch), new_PC);
 
 endmodule
 
 module ProgramCounter
 (
-    input [31:0] in_PC, 
+    input [63:0] new_PC, 
     input clk,
     input rst,
-    output [31:0] out_PC
+    output [63:0] PC
 );
 
-    reg [31:0] out_PC;
+    reg [63:0] update_PC;
+    Update_PC upc(.PC(PC), .new_PC(new_PC));
 
     always @(rst)
     begin
         if(rst)
         begin
-            out_PC = 00000000000001000000000000000000;
+            update_PC = 0000000000000000000000000000000000000000000001000000000000000000;
         end
     end
 
@@ -36,8 +39,9 @@ module ProgramCounter
     begin
         if(!rst)
         begin
-            out_PC = in_PC;
+            update_PC = new_PC;
         end
     end
+    assign PC = update_PC;
 
 endmodule
